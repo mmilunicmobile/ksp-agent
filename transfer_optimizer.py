3b149c74dec2d9a3c5abb3498a425ceec9bec8ac
Matthew Milunic <62996888+mmilunicmobile@users.noreply.github.com>
2025-08-11 20:41:55 -0400

    General two impulse transfer works, three impulse transfer is kinda broken.

--- transfer_optimizer.py
+++ transfer_optimizer.py
@@ -5,7 +5,7 @@ from typing import Callable, Literal
 import numpy as np
 from abc import ABC, abstractmethod
 from astropy.time import Time, TimeDelta
-from scipy.optimize import differential_evolution
+from scipy.optimize import differential_evolution, minimize
 from astropy import units as u
 from astropy.units import Quantity
 
@@ -66,8 +66,8 @@ class TwoOrbitSetLambertTransfer(OptimizableSet):
         self._initial_x = np.concatenate([orbit_set_1.get_x(), orbit_set_2.get_x(), [0.33, 0.66]])
         self._constraints = orbit_set_1.get_constraints() + orbit_set_2.get_constraints() + [(0,1), (0,1)]
     
-        self._x1_len = self._orbit_set_1.get_N()
-        self._x2_len = self._orbit_set_2.get_N()
+        self._x1_end = self._orbit_set_1.get_N()
+        self._x2_end = self._orbit_set_2.get_N() + self._x1_end
 
     def get_x(self):
         return self._initial_x
@@ -76,18 +76,18 @@ class TwoOrbitSetLambertTransfer(OptimizableSet):
         return self._constraints
     
     def get_cost(self, x):
-        x1 = x[0:self._x1_len]
-        x2 = x[self._x1_len:self._x2_len]
+        x1 = x[0:self._x1_end]
+        x2 = x[self._x1_end:self._x2_end]
         
         cost =  self._orbit_set_1.get_cost(x1) + self._orbit_set_2.get_cost(x2) + self.get_maneuver(x).get_total_cost().to_value(u.m / u.s)
         return cost
 
     def get_maneuver(self, x: np.ndarray) -> Maneuver:
-        x1 = x[0:self._x1_len]
-        x2 = x[self._x1_len:self._x2_len]
+        x1 = x[0:self._x1_end]
+        x2 = x[self._x1_end:self._x2_end]
 
         # sort times so that they are always in order
-        times = x[self._x2_len:] * self._maximum_time
+        times = x[self._x2_end:] * self._maximum_time
         times = sorted(times)
         
         # technically this doesnt work
@@ -99,37 +99,32 @@ class TwoOrbitSetLambertTransfer(OptimizableSet):
         return theoretical_manuver
     
     def get_orbit_1(self, x: np.ndarray):
-        x1 = x[0:self._x1_len]
+        x1 = x[0:self._x1_end]
         return self._orbit_set_1.get_orbit(x1)
 
     def get_orbit_1_cost(self, x: np.ndarray):
-        x1 = x[0:self._x1_len]
+        x1 = x[0:self._x1_end]
         return self._orbit_set_1.get_cost(x1)
 
     def get_orbit_2(self, x: np.ndarray):
-        x2 = x[self._x1_len:self._x2_len]
+        x2 = x[self._x1_end:self._x2_end]
         return self._orbit_set_2.get_orbit(x2)
 
     def get_orbit_2_cost(self, x: np.ndarray):
-        x2 = x[self._x1_len:self._x2_len]
+        x2 = x[self._x1_end:self._x2_end]
         return self._orbit_set_2.get_cost(x2)
     
-class TwoOrbitSetTriImpulseTransfer(OptimizableSet):
-    """
-    Is an orbit set that allows minimization of the delta-v required for a Lambert transfer from orbital_set_1 to orbital_set_2 and then from orbital_set_2 to orbital_set_3
-    """
+class TwoOrbitSetProgradeLambertTransfer(OptimizableSet):
     def __init__(self, orbit_set_1: OrbitSet, orbit_set_2: OrbitSet, maximum_time: Quantity):
         self._orbit_set_1 = orbit_set_1
         self._orbit_set_2 = orbit_set_2
         self._maximum_time = maximum_time
 
-        self._initial_x = np.concatenate([orbit_set_1.get_x(), orbit_set_2.get_x(), [100000, 0, 0], [0.33, 0.5, 0.66]])
-        self._constraints = orbit_set_1.get_constraints() + orbit_set_2.get_constraints() + [(50000, 200000), (0, 0), (-180, 180)] + [(0,1), (0,1), (0,1)]
+        self._initial_x = np.concatenate([orbit_set_1.get_x(), orbit_set_2.get_x(), [0, 0.25, 0.50, 0.75]])
+        self._constraints = orbit_set_1.get_constraints() + orbit_set_2.get_constraints() + [(0,10000), (0,1), (0,1), (0,1)]
     
-        self._x2_start = self._orbit_set_1.get_N()
-        self._r1_start = self._orbit_set_2.get_N() + self._x2_start
-        self._t_start = 3 + self._r1_start
-        
+        self._x1_end = self._orbit_set_1.get_N()
+        self._x2_end = self._orbit_set_2.get_N() + self._x1_end
 
     def get_x(self):
         return self._initial_x
@@ -138,37 +133,36 @@ class TwoOrbitSetTriImpulseTransfer(OptimizableSet):
         return self._constraints
     
     def get_cost(self, x):
-        x1 = x[:self._x2_start]
-        x2 = x[self._x2_start:self._r1_start]
+        x1 = x[0:self._x1_end]
+        x2 = x[self._x1_end:self._x2_end]
         
         cost =  self._orbit_set_1.get_cost(x1) + self._orbit_set_2.get_cost(x2) + self.get_maneuver(x).get_total_cost().to_value(u.m / u.s)
         return cost
-
+    
     def get_maneuver(self, x: np.ndarray) -> Maneuver:
-        x1 = x[:self._x2_start]
-        x2 = x[self._x2_start:self._r1_start]
-
-        alt, inc, arglat = x[self._r1_start:self._t_start]
+        x1 = x[0:self._x1_end]
+        x2 = x[self._x1_end:self._x2_end]
 
         # sort times so that they are always in order
-        times = x[self._t_start:] * self._maximum_time
+        impulse = x[self._x2_end] * u.m / u.s
+        times = x[self._x2_end+1:] * self._maximum_time
         times = sorted(times)
-
-        intermediate_orbit = Orbit.circular(Earth, alt=alt*u.m, inc=inc*u.deg, arglat=arglat*u.deg, epoch=Time("J2000", scale="tt") + times[1] + 1 * u.s)
         
         # technically this doesnt work
+
         transfer_start = self._orbit_set_1.get_orbit(x1).propagate(times[0])
+        prograde = transfer_start.v / np.linalg.norm(transfer_start.v)
+        maneuver_a = Maneuver.impulse(prograde * impulse)
+        transfer_middle = transfer_start.apply_maneuver(maneuver_a).propagate(times[1] - times[0] + 1 * u.s) # type: ignore
         transfer_end = self._orbit_set_2.get_orbit(x2).propagate(times[2] + 2 * u.s)
 
-        theoretical_manuver_a = Maneuver.lambert(transfer_start, intermediate_orbit)
-        theoretical_manuver_b = Maneuver.lambert(intermediate_orbit, transfer_end)
+        manuver_b_c = Maneuver.lambert(transfer_middle, transfer_end)
 
-        man_1 = theoretical_manuver_a[0]
-        time_2, dv_2_a = theoretical_manuver_a[1]
-        _, dv_2_b = theoretical_manuver_b[0]
-        man_3 = theoretical_manuver_b[1]
-        
-        return Maneuver(man_1, (time_2, dv_2_a + dv_2_b), man_3)
+        a_dt, a_dv = maneuver_a[0]
+        b_dt, b_dv = manuver_b_c[0]
+        c_dt, c_dv = manuver_b_c[1]
+
+        return Maneuver((times[0], a_dv), (times[1], b_dv), (times[2], c_dv))
 
 def optimize_set(set: OptimizableSet) -> np.ndarray:
     result = differential_evolution(set.get_cost, set.get_constraints(), x0=set.get_x(), tol=0.1)
@@ -179,8 +173,8 @@ def optimize_set(set: OptimizableSet) -> np.ndarray:
 
 def main():
     first_orbit = OrbitSet.from_orbit(Orbit.circular(Earth, 100 * u.km))
-    second_orbit = OrbitSet.from_orbit(Orbit.circular(Earth, 100 * u.km, inc=0 * u.deg))
-    transfer_set = TwoOrbitSetTriImpulseTransfer(first_orbit, second_orbit, 1 * u.day)
+    second_orbit = OrbitSet.from_orbit(Orbit.circular(Earth, 100 * u.km, inc=90 * u.deg))
+    transfer_set = TwoOrbitSetLambertTransfer(first_orbit, second_orbit, 1 * u.day)
     x = optimize_set(transfer_set)
     print(transfer_set.get_maneuver(x))
 
